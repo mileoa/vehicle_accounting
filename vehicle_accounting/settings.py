7d6882baa6efd8e6c44081d938a1bb6c4302a0ef
mileoa <ezkimozzzzz@gmail.com>
2025-05-12 20:43:44 +1000

    change settings

--- vehicle_accounting/settings.py
+++ vehicle_accounting/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("IS_DEBUG", "False").lower() in ["true"]
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
