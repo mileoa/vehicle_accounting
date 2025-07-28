c052ad07d62232417cae6455145beb32868e8c29
mileoa <ezkimozzzzz@gmail.com>
2025-07-28 23:20:30 +1000

    correct tests

--- vehicle_accounting/test_settings.py
+++ vehicle_accounting/test_settings.py
@@ -8,7 +8,7 @@ CACHES = {
 DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.spatialite",
-        'NAME': 'test_db.sqlite3"
+        "NAME": "test_db.sqlite3",
     }
 }
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
